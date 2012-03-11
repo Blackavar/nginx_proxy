095f51ffd95fb76701e8965551b22795042cce4b
Michael Myers <blackavr@gmail.com>
2012-03-10 19:56:51 -0800

    Now depends on nginx and supports ubuntu

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,6 @@ license          "Apache 2.0"
 description      "Configures Nginx to act as a simple proxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+depends          "nginx"
+supports         "ubuntu"
+
