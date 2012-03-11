a84ba2466e2d83e3ea7819f232e7dd3dceac604f
Michael Myers <blackavr@gmail.com>
2012-03-11 12:30:44 -0700

    Corrected the default destination to one that will allow nginx to start

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,4 +5,4 @@ end
 
 default['nginx_proxy']['listen_port']  = "80"
 default['nginx_proxy']['server_name']  = "www.example.com"
-default['nginx_proxy']['destination_url'] = "ws1.example.com"
+default['nginx_proxy']['destination_url'] = "http://127.0.0.1"
