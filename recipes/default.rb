a448311468bb8b00c99d2bf95e5f27ccabfc15d1
Michael Myers <blackavr@gmail.com>
2012-03-11 14:10:01 -0700

    Now restarts nginx after creating config

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,8 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe "nginx"
+
 caching_path = node['nginx_proxy']['conf_dir'] + "/caching.conf"
 template caching_path do
   source "caching.erb"
 end
 
+service "nginx" do
+  action [:restart]
+end
