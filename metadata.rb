69360b35ba79c2beb68e63d3116ba21d99ae55e0
Michael Myers <blackavr@gmail.com>
2012-03-10 17:45:15 -0800

    Updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures nginx-proxy"
+maintainer       "Michael Myers"
+maintainer_email "blackavr@gmail.com"
+license          "Apache 2.0"
+description      "Configures Nginx to act as a simple proxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
